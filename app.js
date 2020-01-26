b2145690f140d1cb798a3a46c2082f9e0cc4e67e
Aditya Ram <adityaram@Adityas-MacBook-Pro.local>
2020-01-25 18:30:49 -0800

    debugging

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var cloudinary = require(cloudinary);
-var cloudinary = require(cloudinary).v2;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -20,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+ 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -31,7 +29,7 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  // set locals, only prov ing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
-const upload = require('./cloudinaryAPI')
+const home = require('./cloudinaryAPI.js')
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', image:upload });
+   res.render('index', { title: 'Express' });
+   console.log("hi")
 });
+router.post('/upload_image',function(req, res, next) {
+  res.send('cloudinaryAPI', { file: image});
+  console.log("what")
+});
+router.post('/uploaded_image', function(req, res, next) {
+  res.send(home.upload())
+  console.log("hello")
+})
 
 module.exports = router;

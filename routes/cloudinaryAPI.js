f59b84d2df806090a69db72cca171095459a5de4
Aditya Ram <adityaram@Adityas-MacBook-Pro.local>
2020-01-27 16:10:07 -0800

    cloudinary config

--- routes/cloudinaryAPI.js
+++ routes/cloudinaryAPI.js
@@ -3,9 +3,9 @@ var cloudinary = require('cloudinary').v2;
 function upload(){
     console.log('test');
     cloudinary.config({
-        cloud_name: 'pictureofbots',
-        api_key: '123146783337381',
-        api_secret: 'lNSZHRwJJ5vprS25VBscSUy2Fe4'
+        cloud_name: 'palyscout',
+        api_key: '281289447429237',
+        api_secret: 'oAoGDruPGJ7Zq0KZZq7LNfqVjg4'
 
     });
     cloudinary.uploader.upload('file', { tags: 'basic_sample' }, function (err, image) { 
